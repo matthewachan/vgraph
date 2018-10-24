c1e832770252f44bf1611603bcbebc0e4902ddc2
Matthew Chan <matthew.a.chan@gmail.com>
2018-10-24 13:12:32 -0400

    Added edges between all convex hull vertices

--- main.py
+++ main.py
@@ -34,14 +34,7 @@ def load_obstacles(object_path):
 	assert len(obstacles)==numObstacles, "number of obstacles does not match the first line"
 	return obstacles
 
-class Vgraph():
-    def __init__(self):
-        rospy.init_node('vgraph', anonymous=False)
-
-        self.marker_pub = rospy.Publisher('vgraph_markerarr', MarkerArray, queue_size=10)
-        r = rospy.Rate(30)
-
-	obstacles = load_obstacles("../data/world_obstacles.txt")
+def grow_obstacles(obstacles):
         grown_obstacles = []
         grown_obstacle = []
 
@@ -60,30 +53,54 @@ class Vgraph():
                 grown_obstacle.append(c4)
             grown_obstacles.append(grown_obstacle)
             grown_obstacle = []
+        return np.asarray(grown_obstacles)
 
-        grown_obstacles = np.asarray(grown_obstacles)
-        # hull = ConvexHull(grown_obstacles[0])
-        # rospy.loginfo(hull.vertices)
+
+def init_marker(marker_id):
+        m = Marker()
+        m.header.frame_id = 'base_link'
+        m.header.stamp = rospy.Time.now()
+        m.id = marker_id
+        m.ns = "ns1"
+        m.action = Marker.ADD;
+        m.pose.orientation.w = 1.0
+
+        m.type = Marker.LINE_LIST
+        m.scale.x = 0.01
+        # m.scale.y = 0.10
+        m.color.g = 1.0
+        m.color.a = 1.0
+        return m
+
+def vertsEqual(v1, v2):
+    return (v1.x == v2.x and v1.y == v2.y) 
+
+class Vgraph():
+    def __init__(self):
+        rospy.init_node('vgraph', anonymous=False)
+
+        self.marker_pub = rospy.Publisher('vgraph_markerarr', MarkerArray, queue_size=10)
+        r = rospy.Rate(30)
+
+	obstacles = load_obstacles("../data/world_obstacles.txt")
+
+        aabb_sidelen = 36
+        half = aabb_sidelen / 2
         scale_factor = 100.0
+
+        grown_obstacles = grow_obstacles(obstacles)
+
         marker_arr = MarkerArray()
-                
+        hull_edges = []
+        hull_verts = [] 
+        marker_id = 0
+        edges = []
 
         while (not rospy.is_shutdown()):
 
             for idx, grown_obstacle in enumerate(grown_obstacles):
-                m = Marker()
-                m.header.frame_id = 'base_link'
-                m.header.stamp = rospy.Time.now()
-                m.id = idx
-                m.ns = "ns1"
-                m.action = Marker.ADD;
-                m.pose.orientation.w = 1.0
-
-                m.type = Marker.LINE_LIST
-                m.scale.x = 0.10
-                m.scale.y = 0.10
-                m.color.g = 1.0
-                m.color.a = 1.0
+                m = init_marker(marker_id)
+                marker_id += 1
                 points = []
                 hull = ConvexHull(grown_obstacle)
                 num_points = len(hull.vertices)
@@ -94,7 +111,6 @@ class Vgraph():
                     p1.x = grown_obstacle[vertices[i]][0] / scale_factor
                     p1.y = grown_obstacle[vertices[i]][1] / scale_factor
                     p1.z = 0
-                    points.append(p1)
                     # Append next vertex (or start vertex)
                     p2 = Point()
                     p2.x = 0
@@ -106,10 +122,29 @@ class Vgraph():
                         p2.x = grown_obstacle[vertices[i + 1]][0] / scale_factor
                         p2.y = grown_obstacle[vertices[i + 1]][1] / scale_factor
                     p2.z = 0
+
+                    points.append(p1)
                     points.append(p2)
+
+                    hull_verts.append(p1)
+                    hull_edges.append([p1, p2])
 
                 m.points = points
                 marker_arr.markers.append(m)
+
+            m = init_marker(marker_id)
+            marker_id += 1
+            points = []
+            for v1 in hull_verts:
+                for v2 in hull_verts:
+                    if (not vertsEqual(v1, v2)):
+                        points.append(v1)
+                        points.append(v2)
+            m.points = points
+            marker_arr.markers.append(m)
+            
+                       
+
 
         
             self.marker_pub.publish(marker_arr)
