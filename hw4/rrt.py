f94d7a98415d1bd4075f7826dcea6d722767488a
Matthew Chan <matthew.a.chan@gmail.com>
2018-11-12 20:22:06 -0500

    Initialize RRT

--- hw4/rrt.py
+++ hw4/rrt.py
@@ -9,6 +9,28 @@ from matplotlib.path import Path
 import matplotlib.patches as patches
 import numpy as np
 import random, math
+import time
+
+# Node class
+class Node:
+    def __init__(self, point, parent):
+        self.point = point
+        self.parent = parent
+
+def getDistance(a, b):
+    return np.sqrt(np.power(a[0] - b[0], 2) + np.power(a[1] - b[1], 2))
+
+# Points are tuples
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def getX(self):
+#         return self.x
+
+#     def getY(self):
+#         return self.y
 
 def build_obstacle_course(obstacle_path, ax):
     vertices = list()
@@ -60,5 +82,39 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
     path = build_obstacle_course(args.obstacle_path, ax)
     start, goal = add_start_and_goal(args.start_goal_path, ax)
+    
+    # Initialize RRT
+    rrt = Node(start, None)
+    nodeList = [start]
 
+    # Initialize constants
+    stepLen = 10 # Default step length
+    bounds = (600, 600) # Default bounds of 2D world space
+    random.seed(time.time())
+
+    # Generate q-rand
+    qRand = (random.randint(0, bounds[0]), random.randint(0, bounds[1]))
+
+    # Locate the closet node to q-rand in the RRT
+    closestNode = nodeList[0]
+    minDist = getDistance(qRand, closestNode)
+
+    for node in nodeList:
+        if getDistance(qRand, node) < minDist:
+            closestNode = node
+            minDist = getDistance(qRand, node)
+
+    # Locate q-new
+    qNew = ()
+    if getDistance(qRand, closestNode) < stepLen:
+        qNew = qRand
+    else:
+        # Linear algebra to find a point a given distance away on a line
+        v = (qRand[0] - closestNode[0], qRand[1] - closestNode[1])
+        norm = np.sqrt(np.power(v[0], 2) + np.power(v[1], 2))
+        u = (v[0] / norm, v[1] / norm)
+        qNew = (closestNode[0] + stepLen * u[0], closestNode[1] + stepLen * u[1])
+
+    # Check q-new for collisions
+    
     plt.show()
